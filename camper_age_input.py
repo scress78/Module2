3f11ace2d2e6066bbadcbd985ed54e7aa6b9c8e2
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-02 17:07:25 -0500

    Add files via upload

--- camper_age_input.py
+++ camper_age_input.py
@@ -1,1 +1,21 @@
-if __name__ == '__main__':
+"""
+This program converts a childs age in months into years and then outputs
+how old they are in months and years old.
+
+Author: Spencer Cress
+Date: 06/02/2020
+"""
+
+def convert_to_months(x):
+    return int(x) / 12
+
+if __name__ == "__main__":
+    age_in_months = input("Enter child's age in months: ")
+    ages_in_years = convert_to_months(age_in_months)
+
+if __name__ == "__main__":
+    print(str(age_in_months)+' months is '+str(ages_in_years)+' years old')
+
+#I think this finally works
+#it has taken two more hours than I thought it would :)
+
