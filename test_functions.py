7c3d99df64dfb87929f43bbd994a0bb131cf14fa
scress78 <65985833+scress78@users.noreply.github.com>
2020-06-02 17:07:01 -0500

    Add files via upload

--- test_functions.py
+++ test_functions.py
@@ -1,15 +1,11 @@
 import unittest
 from main import camper_age_input
 
-class FunctionTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(False, True)
+
+class MyTestCase(unittest.TestCase):
+    def test_convert_to_months(self):
+        self.assertEqual(camper_age_input.convert_to_months(12), 1)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-def convert_to_months(x):
-    pass
-
-if __name__ == '__main__':
